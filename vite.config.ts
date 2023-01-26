0c84c6cc5833a465a02e7d2cf0ffe68c08477cec
Marco T O Valeriano <marco.valeriano@bancointer.com.br>
2023-01-26 15:36:14 -0300

    removed

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/ignite-2022-reactjs-coffee-challenge-02-delivery',
-})
